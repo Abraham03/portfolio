86257759980c9f6ed1f29b5a07ddf1c21f71cddf
Abraham Chavez <achave8627@gmail.com>
2023-08-31 09:52:12 -0600

    Se agrego el scroll smooth y se agrego href a los menus.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+
+    // Manejo del clic en el enlace del menú
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+          
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+
 function toggleMenu() {
     var navbarCollapse = document.getElementById("navbarCollapse");
     navbarCollapse.classList.toggle("active");
